d9436e4092fe88478340cf3520a27926c6270545
jmpichardo <juanma.pichardo@gmail.com>
2018-10-17 16:27:53 +0100

    Added example job and service to get injected (#5)

---
+++ ASPNetCoreHangfire/Jobs/ExampleJob.cs
@@ -0,0 +1,25 @@
+﻿using ASPNetCoreHangfire.Services.Users;
+using System;
+
+namespace ASPNetCoreHangfire.Jobs
+{
+    public class ExampleJob
+    {
+        private readonly IUsersService _usersService;
+
+        private Random rand = new Random();
+
+        public ExampleJob(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        public void Run()
+        {
+            var names = _usersService.GetNames();
+            var pickedName = names[rand.Next(names.Count)];
+
+            System.Diagnostics.Debug.WriteLine($"The job is running now and my picked name is {pickedName}");
+        }
+    }
+}
---
+++ ASPNetCoreHangfire/Services/Users/UsersService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreHangfire.Services.Users
+{
+    public class UsersService : IUsersService
+    {
+        public List<string> GetNames()
+        {
+            return new List<string>()
+            {
+                "Juan",
+                "Shane",
+                "Nick",
+                "Damilola",
+                "Karim",
+                "Carl",
+                "Huseyin",
+            };
+        }
+    }
+}
