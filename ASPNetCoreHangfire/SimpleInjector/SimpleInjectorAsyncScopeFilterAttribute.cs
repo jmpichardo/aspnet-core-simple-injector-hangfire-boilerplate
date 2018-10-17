991753133d08c754e2ef4bcab9323b0b005735c6
jmpichardo <juanma.pichardo@gmail.com>
2018-10-17 15:29:13 +0100

    Integrated Hangfire with Simple Injector in ASP.Net Core project (#2)

---
+++ ASPNetCoreHangfire/SimpleInjector/SimpleInjectorAsyncScopeFilterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Hangfire.Common;
+using Hangfire.Server;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+
+namespace ASPNetCoreHangfire.SimpleInjector
+{
+    public class SimpleInjectorAsyncScopeFilterAttribute : JobFilterAttribute, IServerFilter
+    {
+        private static readonly AsyncScopedLifestyle lifestyle = new AsyncScopedLifestyle();
+
+        private readonly Container _container;
+
+        public SimpleInjectorAsyncScopeFilterAttribute(Container container)
+        {
+            _container = container;
+        }
+
+        public void OnPerforming(PerformingContext filterContext)
+        {
+            AsyncScopedLifestyle.BeginScope(_container);
+        }
+
+        public void OnPerformed(PerformedContext filterContext)
+        {
+            var scope = lifestyle.GetCurrentScope(_container);
+            if (scope != null)
+                scope.Dispose();
+        }
+    }
+}
---
+++ ASPNetCoreHangfire/SimpleInjector/SimpleInjectorJobActivator.cs
@@ -0,0 +1,56 @@
+﻿using Hangfire;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using System;
+
+namespace ASPNetCoreHangfire.SimpleInjector
+{
+    public class SimpleInjectorJobActivator : JobActivator
+    {
+        public Container Container { get; }
+        public ScopedLifestyle Lifestyle { get; }
+
+        public SimpleInjectorJobActivator(Container container)
+        {
+            Lifestyle = new AsyncScopedLifestyle();
+            container.Options.DefaultScopedLifestyle = Lifestyle;
+
+            Container = container;
+        }
+
+        public override object ActivateJob(Type type)
+        {
+            return Container.GetInstance(type);
+        }
+
+        public override JobActivatorScope BeginScope(JobActivatorContext c)
+        {
+            return new SimpleInjectorScope(Container, Lifestyle.GetCurrentScope(Container));
+        }
+
+        private sealed class SimpleInjectorScope : JobActivatorScope
+        {
+            private readonly Container _container;
+            private readonly Scope _scope;
+
+            public SimpleInjectorScope(Container container, Scope scope)
+            {
+                _container = container;
+                _scope = scope;
+            }
+
+            public override object Resolve(Type type)
+            {
+                return _container.GetInstance(type);
+            }
+
+            public override void DisposeScope()
+            {
+                if (_scope != null)
+                {
+                    _scope.Dispose();
+                }
+            }
+        }
+    }
+}
